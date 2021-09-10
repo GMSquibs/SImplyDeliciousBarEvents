19386828b253e2b0af641ddf08cb93ea57ccb5c9
GMSquibs <89954806+GMSquibs@users.noreply.github.com>
2021-09-10 14:24:06 -0500

    Recreated controllers

---
+++ SimplyDeliciousBarEvents/Models/Invoice.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SimplyDeliciousBarEvents.Models
+{
+    public class Invoice
+    {
+        [Key]
+        public int Id { get; set; }
+
+        public int EventId {get;set;}
+        public int MenuId { get; set; }
+
+        public int Quantity { get; set; }
+        public decimal CalculatedCost { get; set; }
+    }
+}
